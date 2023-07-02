c318652a156f0a56a79b73a5c33757f4fa0655c9
Serdtseva Anna <anna.serdtseva@rtmis.ru>
2023-07-02 15:38:15 +0700

    3.7 (Проверочная работа 1.2) - остановка аукциона с возвратом последней принятой заявки
    Signed-off-by: Serdtseva Anna <anna.serdtseva@rtmis.ru>

--- src/main/java/course/concurrency/exams/auction/AuctionStoppableOptimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionStoppableOptimistic.java
@@ -1,29 +1,43 @@
 package course.concurrency.exams.auction;
 
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
 public class AuctionStoppableOptimistic implements AuctionStoppable {
 
-    private Notifier notifier;
+    private final Notifier notifier;
+    private final AtomicMarkableReference<Bid> latestBid;
 
     public AuctionStoppableOptimistic(Notifier notifier) {
         this.notifier = notifier;
+        this.latestBid = new AtomicMarkableReference<>(new Bid(0L, 0L, 0L), true);
     }
 
-    private Bid latestBid;
-
     public boolean propose(Bid bid) {
-        if (bid.getPrice() > latestBid.getPrice()) {
-            notifier.sendOutdatedMessage(latestBid);
-            latestBid = bid;
-            return true;
-        }
-        return false;
+        Bid currentLatestBid;
+        do {
+            if (!latestBid.isMarked()) {
+                return false;
+            }
+            currentLatestBid = latestBid.getReference();
+            if (currentLatestBid.getPrice() >= bid.getPrice()) {
+                return false;
+            }
+        } while (!latestBid.compareAndSet(currentLatestBid, bid, true, true));
+
+        notifier.sendOutdatedMessage(currentLatestBid);
+        return true;
     }
 
     public Bid getLatestBid() {
-        return latestBid;
+        return latestBid.getReference();
     }
 
     public Bid stopAuction() {
-        return latestBid;
+        Bid currentLatestBid = latestBid.getReference();
+        if (!latestBid.isMarked()) {
+            return currentLatestBid;
+        }
+        latestBid.set(currentLatestBid, false);
+        return currentLatestBid;
     }
 }
