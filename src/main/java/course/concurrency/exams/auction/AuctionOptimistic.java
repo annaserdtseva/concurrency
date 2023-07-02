3fec56f917d3ef31f1cd77c429860b59c0d20f5a
Serdtseva Anna <anna.serdtseva@rtmis.ru>
2023-07-02 14:41:02 +0700

    3.7 (Проверочная работа 1.1) - реализовала пессимистичную блокировку в AuctionPessimistic - реализовала оптимистичную блокировку в AuctionOptimistic - Notifier не блокирует работу
    Signed-off-by: Serdtseva Anna <anna.serdtseva@rtmis.ru>

--- src/main/java/course/concurrency/exams/auction/AuctionOptimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionOptimistic.java
@@ -1,25 +1,31 @@
 package course.concurrency.exams.auction;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class AuctionOptimistic implements Auction {
 
-    private Notifier notifier;
+    private final Notifier notifier;
+    private final AtomicReference<Bid> latestBid;
 
     public AuctionOptimistic(Notifier notifier) {
         this.notifier = notifier;
+        this.latestBid = new AtomicReference<>(new Bid(0L, 0L, 0L));
     }
 
-    private Bid latestBid;
-
     public boolean propose(Bid bid) {
-        if (bid.getPrice() > latestBid.getPrice()) {
-            notifier.sendOutdatedMessage(latestBid);
-            latestBid = bid;
-            return true;
-        }
-        return false;
+        Bid currentLatestBid;
+        do {
+            currentLatestBid = latestBid.get();
+            if (currentLatestBid.getPrice() >= bid.getPrice()) {
+                return false;
+            }
+        } while(!latestBid.compareAndSet(currentLatestBid, bid));
+
+        notifier.sendOutdatedMessage(currentLatestBid);
+        return true;
     }
 
     public Bid getLatestBid() {
-        return latestBid;
+        return latestBid.get();
     }
 }
