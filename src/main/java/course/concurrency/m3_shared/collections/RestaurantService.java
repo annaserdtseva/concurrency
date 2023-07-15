1b41bef985f70ec28a214cef8b72a236337bdf3f
Serdtseva Anna <anna.serdtseva@rtmis.ru>
2023-07-15 17:29:40 +0700

    3.11 - выбрала тип для stat - реализовала логику подсчёта и выдачи статистики по ресторанам
    Signed-off-by: Serdtseva Anna <anna.serdtseva@rtmis.ru>

--- src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
+++ src/main/java/course/concurrency/m3_shared/collections/RestaurantService.java
@@ -1,19 +1,28 @@
 package course.concurrency.m3_shared.collections;
 
-import java.util.HashSet;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.stream.Collectors;
 
 public class RestaurantService {
 
-    private Map<String, Restaurant> restaurantMap = new ConcurrentHashMap<>() {{
+    static final String STAT_FORMAT = "%s - %d";
+    private final Map<String, Restaurant> restaurantMap = new ConcurrentHashMap<>() {{
         put("A", new Restaurant("A"));
         put("B", new Restaurant("B"));
         put("C", new Restaurant("C"));
     }};
 
-    private Object stat;
+    private final ConcurrentHashMap<String, LongAdder> stat;
+
+    public RestaurantService() {
+        this.stat = new ConcurrentHashMap<>(
+            restaurantMap.entrySet().stream()
+                .collect(Collectors.toMap(Entry::getKey, v -> new LongAdder())));
+    }
 
     public Restaurant getByName(String restaurantName) {
         addToStat(restaurantName);
@@ -21,11 +30,12 @@ public class RestaurantService {
     }
 
     public void addToStat(String restaurantName) {
-        // your code
+        stat.get(restaurantName).increment();
     }
 
     public Set<String> printStat() {
-        // your code
-        return new HashSet<>();
+        return stat.entrySet().stream()
+            .map(v -> String.format(STAT_FORMAT, v.getKey(), v.getValue().longValue()))
+            .collect(Collectors.toSet());
     }
 }
