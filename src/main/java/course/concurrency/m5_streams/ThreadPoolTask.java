717a608090b14a2198888d8066bc5bf3a5fab176
Serdtseva Anna <anna.serdtseva@rtmis.ru>
2023-07-29 15:23:00 +0700

    5.2 (экзекьюторы и очереди)
    Signed-off-by: Serdtseva Anna <anna.serdtseva@rtmis.ru>

--- src/main/java/course/concurrency/m5_streams/ThreadPoolTask.java
+++ src/main/java/course/concurrency/m5_streams/ThreadPoolTask.java
@@ -1,16 +1,21 @@
 package course.concurrency.m5_streams;
 
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor.DiscardPolicy;
+import java.util.concurrent.TimeUnit;
 
 public class ThreadPoolTask {
 
     // Task #1
     public ThreadPoolExecutor getLifoExecutor() {
-        return null;
+        return new ThreadPoolExecutor(0, Integer.MAX_VALUE, 0L, TimeUnit.MILLISECONDS,
+            new CustomLifoBlockingQueue<>());
     }
 
     // Task #2
     public ThreadPoolExecutor getRejectExecutor() {
-        return null;
+        return new ThreadPoolExecutor(0, 8, 0L, TimeUnit.MILLISECONDS,
+            new SynchronousQueue<>(), new DiscardPolicy());
     }
 }
